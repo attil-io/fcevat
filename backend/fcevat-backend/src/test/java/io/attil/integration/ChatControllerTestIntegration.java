bc443b4159149e92c41e7b09442c05880d7601cf
Attilio <parrotchaffinch@gmail.com>
2017-05-13 15:37:22 +0200

    Integration test with rest assured.

---
+++ backend/fcevat-backend/src/test/java/io/attil/integration/ChatControllerTestIntegration.java
@@ -0,0 +1,25 @@
+package io.attil.integration;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ChatControllerTestIntegration {
+
+	@Before
+	public void setup() {
+		RestAssured.baseURI = "http://172.17.0.2:8080/fcevat";
+	}
+	
+	@Test
+	public void testAllMessages() {
+		expect()
+			.statusCode(200)
+			.body(equalTo("[{\"text\":\"hello\"},{\"text\":\"hello\"},{\"text\":\"hello\"},{\"text\":\"hello\"},{\"text\":\"hello\"}]"))
+		.given()
+		.when().post("/messages");
+	}
+}
