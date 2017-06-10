644fd6262f6fcd570930e886564e86791b0e98d6
Attilio <parrotchaffinch@gmail.com>
2017-06-10 22:04:46 +0200

    implementation of messages-list

--- frontend/angular2-fe/src/app/messages-list/messages-list.component.ts
+++ frontend/angular2-fe/src/app/messages-list/messages-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message';
 
 @Component({
   selector: 'app-messages-list',
   template: `
-    <p>
-      messages-list Works!
-    </p>
-  `,
+  <ul>
+    <li *ngFor="let message of messages">
+      {{message.text}}
+    </li>
+  </ul>
+
+ `,
   styleUrls: ['./messages-list.component.scss']
 })
 export class MessagesListComponent implements OnInit {
+  messages: Message[] = [
+    {text: 'Hello, world!'},
+    {text: 'Nice day, today!'},
+    {text: 'Oh, yeah!'},
+  ];
 
   constructor() { }
 
