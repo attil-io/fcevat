9c3857aa512f485cdbba457da0e23bd62e0a3a54
Attilio <parrotchaffinch@gmail.com>
2017-06-10 22:20:17 +0200

    Implement message service.

--- frontend/angular2-fe/src/app/messages.service.ts
+++ frontend/angular2-fe/src/app/messages.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Message } from './message';
 
 @Injectable()
 export class MessagesService {
 
   constructor() { }
+
+  getAll() : Message[] {
+    return [
+        {text: 'Hello, world!'},
+        {text: 'Nice day, today!'},
+        {text: 'Oh, yeah!'},
+    ];
+  }
 
 }
