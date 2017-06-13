8e81b6a5408860708692036776bb1e8e2ef1ed01
Attilio <parrotchaffinch@gmail.com>
2017-06-13 20:30:45 +0200

    message details component

--- frontend/angular2-fe/src/app/app.module.ts
+++ frontend/angular2-fe/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MessagesListComponent } from './messages-list/messages-list.component';
+import { MessageDetailsComponent } from './message-details/message-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesListComponent
+    MessagesListComponent,
+    MessageDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/angular2-fe/src/app/message-details/message-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-details',
+  template: `
+    <p>
+      message-details Works!
+    </p>
+  `,
+  styles: []
+})
+export class MessageDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
