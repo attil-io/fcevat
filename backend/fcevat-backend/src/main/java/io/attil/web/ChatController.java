078bf19021f849405ae6c7bca7f2b5a3485348be
Attilio <parrotchaffinch@gmail.com>
2017-06-15 22:15:22 +0200

    Enable CORS on the back-end.

--- backend/fcevat-backend/src/main/java/io/attil/web/ChatController.java
+++ backend/fcevat-backend/src/main/java/io/attil/web/ChatController.java
@@ -3,6 +3,7 @@ package io.attil.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class ChatController {
 	@Autowired
 	private ChatService chatService;
 	
+        @CrossOrigin
 	@RequestMapping(value="/messages", produces="application/json")
 	public List<Message> getMessages() {
 		List<Message> messages = chatService.getMessages();
