c178cf201cbd9bdca7017228a9c3885fee847500
Attilio <parrotchaffinch@gmail.com>
2017-06-26 22:17:15 +0200

    [BE] New endpoint for writing messages.

--- backend/fcevat-backend/src/main/java/io/attil/web/ChatController.java
+++ backend/fcevat-backend/src/main/java/io/attil/web/ChatController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import io.attil.domain.Message;
 import io.attil.service.ChatService;
 
@@ -21,5 +23,11 @@ public class ChatController {
 	public List<Message> getMessages() {
 		List<Message> messages = chatService.getMessages();
 		return messages;
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value="/write", method=POST)
+	public void write() {
+		System.out.println("XXX WRITE!");
 	}
 }
